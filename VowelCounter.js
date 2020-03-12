312574b7d7316b92c7216ac897a816c3743eaa0e
Jamal <moulayjam@gmail.com>
2020-03-12 13:37:36 -0400

    Vowel Counter Algo With Solution

---
+++ VowelCounter.js
@@ -0,0 +1,15 @@
+// =============== [Vowel Counter Algo] ================
+
+
+const vowelCounter = str => {
+  let counter = 0;
+  let checker = ['a', 'e', 'i', 'o', 'u']
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+vowelCounter('jamalino koko chino')
