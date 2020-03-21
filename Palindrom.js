7da55a00e61e255db0ac42848ddd738232a3c7ae
Jamal <moulayjam@gmail.com>
2020-03-21 12:59:01 -0400

    Palindrom Algo With Solution

---
+++ Palindrom.js
@@ -0,0 +1,9 @@
+
+// =============== [Palindrom Algo] ==================
+
+
+const palindrome = str => {
+    return str.toLowerCase().split('').reverse().join('') === str.toLowerCase()
+  }
+  
+  palindrome('RaceCar')
