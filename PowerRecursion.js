ad5ba1ec2587c49b416d3bc91f8c28dfc821101a
Jamal <moulayjam@gmail.com>
2020-03-21 21:34:15 -0400

    PowerRecursion Algo With Solution

---
+++ PowerRecursion.js
@@ -0,0 +1,12 @@
+
+// =============== [PowerRecursion Algo] ================
+
+
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base,exponent-1);
+}
+
+
+power(2, 2)
+
