6b4765203b2afc50dd2b74305f8c82dc674a5b08
Jamal <moulayjam@gmail.com>
2020-03-21 21:29:49 -0400

    RecursiveRange Algo With Solution

---
+++ recursiveRange.js
@@ -0,0 +1,12 @@
+
+// Write a function called recursiveRange which accepts a number and adds up 
+// all the numbers from 0 to the number passed to the function 
+
+
+
+const recursiveRange = x => {
+    if (x === 0 ) return 0;
+    return x + recursiveRange(x-1);
+ }
+
+recursiveRange(12)
