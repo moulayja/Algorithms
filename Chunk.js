f17136a9afa3f55a19109f29d84c808b4d277541
Jamal <moulayjam@gmail.com>
2020-03-21 21:17:01 -0400

    Chunk Algo With Solution

---
+++ Chunk.js
@@ -0,0 +1,18 @@
+
+
+// =============== [Chunk Algo] ================
+
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+  
+    while (index < array.length) {
+      chunked.push(array.slice(index, index + size));
+      index += size;
+    }
+  
+    return chunked;
+  }
+
+
+  
